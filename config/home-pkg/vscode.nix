2bf3ff043d3c7314a996a69095e3dc0b9b2d51ff
Duskell <duskell@proton.me>
2025-11-02 13:18:24 +0100

    changed vscode user settings

--- config/home-pkg/vscode.nix
+++ config/home-pkg/vscode.nix
@@ -9,6 +9,8 @@
       "workbench.iconTheme" = "material-icon-theme";
       "editor.fontFamily" = "JetBrains Mono";
       "editor.fontLigatures" = true;
+      "git.enableSmartCommit": true,
+      "git.confirmSync": false
     };
 
     profiles.default.extensions = with pkgs.vscode-extensions; [
