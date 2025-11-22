769bfb40e62a0c3711069a2fa5ad66c053f3f95e
Duskell <duskell@proton.me>
2025-11-22 18:31:54 +0100

    added 3210 port to tcp on server

--- hosts/server/configuration.nix
+++ hosts/server/configuration.nix
@@ -34,7 +34,7 @@
     enable = true;
     trustedInterfaces = [ "tailscale0" ];
     allowedUDPPorts = [ config.services.tailscale.port ];
-    allowedTCPPorts = [ 80 22 443 25565 config.services.tailscale.port ];
+    allowedTCPPorts = [ 80 22 443 25565 3210 config.services.tailscale.port ];
     interfaces.podman1 = {
       allowedUDPPorts = [ 53 ]; # this needs to be there so that containers can look eachother's names up over DNS
     };
