07331ea90570c0165731d6d69b22d45b0f5acb42
Duskell <duskell@proton.me>
2025-11-27 17:55:29 +0100

    forgot to change the port on copyparty to 3200

--- config/system-pkg/copyparty.nix
+++ config/system-pkg/copyparty.nix
@@ -1,87 +1,91 @@
-{ config, pkgs, lib, ... }:
-
 {
-    environment.systemPackages = [ pkgs.copyparty ];
+  config,
+  pkgs,
+  lib,
+  ...
+}: {
+  environment.systemPackages = [pkgs.copyparty];
 
-    services.copyparty = {
-        enable = true;
-        user = "copyparty"; 
-        group = "copyparty"; 
-        # directly maps to values in the [global] section of the copyparty config.
-        # see `copyparty --help` for available options
-        settings = {
-            i = "0.0.0.0"; #ip
-            p = [ 3210 ]; # ports
-            # use booleans to set binary flags
-            no-reload = true;
-            shr = "/public";
+  services.copyparty = {
+    enable = true;
+    user = "copyparty";
+    group = "copyparty";
+    # directly maps to values in the [global] section of the copyparty config.
+    # see `copyparty --help` for available options
+    settings = {
+      i = "0.0.0.0"; #ip
+      p = [3200]; # ports
+      # use booleans to set binary flags
+      no-reload = true;
+      shr = "/public";
 
-            # reverse-proxy / Cloudflare real-IP handling
-            "xff-hdr" = "cf-connecting-ip";   # header CF fills with the client IP
-            "xff-src" = "any";                # or list only the CF CIDRs you allow through
-            rproxy = 1;        
-        };
-
-        accounts = {
-            # specify the account name as the key
-            levente.passwordFile = "/var/lib/copyparty/levente_password";
-
-            attila.passwordFile = "/var/lib/copyparty/attila_password";
-        };
-
-        groups = {
-            owner = [ "levente" ];
-            users = [ "attila" ];
-        };
-
-        volumes = {
-            "/" = {
-            path = "/srv/copyparty/private";
-            # see `copyparty --help-accounts` for available options
-            access = {
-                # r = "*";
-                rwmd = [ "levente" "attila" ];
-                rw = [ "attila" ];
-            };
-            # see `copyparty --help-flags` for available options
-            flags = {
-                # "fk" enables filekeys (necessary for upget permission) (4 chars long)
-                fk = 4;
-                # scan for new files every 60sec
-                scan = 60;
-                # volflag "e2d" enables the uploads database
-                e2d = true;
-                # "d2t" disables multimedia parsers (in case the uploads are malicious)
-                d2t = true;
-                # skips hashing file contents if path matches *.iso
-                nohash = "\.iso$";
-            };
-            };
-
-            "/public" = {
-            path = "/srv/copyparty/public";
-            # see `copyparty --help-accounts` for available options
-            access = {
-                # r = "*";
-                rwmd = [ "levente" "attila" ];
-                r = [ "attila" ];
-            };
-            # see `copyparty --help-flags` for available options
-            flags = {
-                # "fk" enables filekeys (necessary for upget permission) (4 chars long)
-                fk = 4;
-                # scan for new files every 60sec
-                scan = 60;
-                # volflag "e2d" enables the uploads database
-                e2d = true;
-                # "d2t" disables multimedia parsers (in case the uploads are malicious)
-                d2t = true;
-                # skips hashing file contents if path matches *.iso
-                nohash = "\.iso$";
-            };
-            };
-        };
-        # you may increase the open file limit for the process
-        openFilesLimit = 8192;
+      # reverse-proxy / Cloudflare real-IP handling
+      "xff-hdr" = "cf-connecting-ip"; # header CF fills with the client IP
+      "xff-src" = "any"; # or list only the CF CIDRs you allow through
+      rproxy = 1;
     };
+
+    accounts = {
+      # specify the account name as the key
+      levente.passwordFile = "/var/lib/copyparty/levente_password";
+
+      attila.passwordFile = "/var/lib/copyparty/attila_password";
+    };
+
+    groups = {
+      owner = ["levente"];
+      users = ["attila"];
+    };
+
+    volumes = {
+      "/" = {
+        path = "/srv/copyparty/private";
+        # see `copyparty --help-accounts` for available options
+        access = {
+          # r = "*";
+          rwmd = ["levente" "attila"];
+          rw = ["attila"];
+        };
+        # see `copyparty --help-flags` for available options
+        flags = {
+          # "fk" enables filekeys (necessary for upget permission) (4 chars long)
+          fk = 4;
+          # scan for new files every 60sec
+          scan = 60;
+          # volflag "e2d" enables the uploads database
+          e2d = true;
+          # "d2t" disables multimedia parsers (in case the uploads are malicious)
+          d2t = true;
+          # skips hashing file contents if path matches *.iso
+          nohash = "\.iso$";
+        };
+      };
+
+      "/public" = {
+        path = "/srv/copyparty/public";
+        # see `copyparty --help-accounts` for available options
+        access = {
+          # r = "*";
+          rwmd = ["levente" "attila"];
+          r = ["attila"];
+        };
+        # see `copyparty --help-flags` for available options
+        flags = {
+          # "fk" enables filekeys (necessary for upget permission) (4 chars long)
+          fk = 4;
+          # scan for new files every 60sec
+          scan = 60;
+          # volflag "e2d" enables the uploads database
+          e2d = true;
+          # "d2t" disables multimedia parsers (in case the uploads are malicious)
+          d2t = true;
+          # skips hashing file contents if path matches *.iso
+          nohash = "\.iso$";
+        };
+      };
+    };
+    # you may increase the open file limit for the process
+    openFilesLimit = 8192;
+  };
 }
+
