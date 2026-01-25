8e77580a79dd0041b79ae7e037ba1f96cdb1c2de
Duskell <duskell@proton.me>
2026-01-25 11:14:46 +0100

    modified copyparty settings

--- config/system-pkg/copyparty.nix
+++ config/system-pkg/copyparty.nix
@@ -73,29 +73,6 @@
           norobots = true;
         };
       };
-
-      "/" = {
-        path = "/srv/copyparty/public";
-        # see `copyparty --help-accounts` for available options
-        access = {
-          # r = "*";
-          rwmd = ["levente" "attila"];
-          r = ["attila"];
-        };
-        # see `copyparty --help-flags` for available options
-        flags = {
-          # "fk" enables filekeys (necessary for upget permission) (4 chars long)
-          fk = 4;
-          # scan for new files every 60sec
-          scan = 60;
-          # volflag "e2d" enables the uploads database
-          e2d = true;
-          # "d2t" disables multimedia parsers (in case the uploads are malicious)
-          d2t = true;
-          # skips hashing file contents if path matches *.iso
-          nohash = "\.iso$";
-        };
-      };
     };
     # you may increase the open file limit for the process
     openFilesLimit = 8192;
